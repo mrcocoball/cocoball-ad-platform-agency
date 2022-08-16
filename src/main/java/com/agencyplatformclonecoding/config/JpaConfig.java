610ac01bc21f4a9b1da74dfdbde7e9216f4aaade
mrcocoball <gunmaru93@gmail.com>
2022-08-16 23:17:26 +0900

    #13 - 디렉토리명 리팩토링

--- src/main/java/com/agencyplatformclonecoding/config/JpaConfig.java
+++ src/main/java/com/agencyplatformclonecoding/config/JpaConfig.java
@@ -1,4 +1,4 @@
-package com.agencyplatformclonecoding.Config;
+package com.agencyplatformclonecoding.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
