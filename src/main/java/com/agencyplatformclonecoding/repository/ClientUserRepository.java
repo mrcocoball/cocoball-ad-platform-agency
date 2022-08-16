075e781edf8e8a4c241888e64e3dbfcb8a5fb800
mrcocoball <gunmaru93@gmail.com>
2022-08-16 23:19:23 +0900

    #13 - 추후 검색 기능을 위한 Repository 수정
    검색 기능을 위해 각 Repository에 Querydsl 관련 메소드를 추가하였음

--- src/main/java/com/agencyplatformclonecoding/repository/ClientUserRepository.java
+++ src/main/java/com/agencyplatformclonecoding/repository/ClientUserRepository.java
@@ -1,7 +1,25 @@
 package com.agencyplatformclonecoding.repository;
 
 import com.agencyplatformclonecoding.domain.ClientUser;
+import com.agencyplatformclonecoding.domain.QClientUser;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-public interface ClientUserRepository extends JpaRepository<ClientUser, String> {
+public interface ClientUserRepository extends
+        JpaRepository<ClientUser, String>,
+        QuerydslPredicateExecutor<ClientUser>,
+        QuerydslBinderCustomizer<QClientUser> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QClientUser root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.userId, root.nickname, root.createdAt, root.createdBy);
+        bindings.bind(root.nickname).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
