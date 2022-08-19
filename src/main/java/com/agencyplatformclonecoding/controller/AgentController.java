e9c7323374e4b49a257d361f9a45b8a4bffa9cee
mrcocoball <gunmaru93@gmail.com>
2022-08-19 19:57:25 +0900

    #17 - 컨트롤러, 서비스, 테스트 추가
    * 각 뷰를 조회하는 기능과 이에 대한 테스트를 구현하였고 서비스는 임의로 구현해둔 상황

--- src/main/java/com/agencyplatformclonecoding/controller/AgentController.java
+++ src/main/java/com/agencyplatformclonecoding/controller/AgentController.java
@@ -1,4 +1,40 @@
 package com.agencyplatformclonecoding.controller;
 
+import com.agencyplatformclonecoding.service.AgentService;
+import com.agencyplatformclonecoding.service.PaginationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("/agents")
+@Controller
 public class AgentController {
+
+    private final AgentService agentService;
+    private final PaginationService paginationService;
+
+    @GetMapping
+    public String agents(ModelMap map) {
+        map.addAttribute("agents", List.of());
+        return "agents/index";
+    }
+
+    @GetMapping("/{agentId}")
+    public String agent(@PathVariable String agentId, ModelMap map) {
+        map.addAttribute("agent", "agent"); // TODO : 실제 데이터 구현 시 여기에 넣어야 함
+        map.addAttribute("clients", List.of());
+         return "agents/detail";
+    }
+    @GetMapping("/{agentId}/{clientId}")
+    public String mappingClient(@PathVariable String agentId, String clientId) {
+         return "forward:/manage/{clientId}";
+     }
+
 }
--- src/test/java/com/agencyplatformclonecoding/controller/AgentControllerTest.java
+++ src/test/java/com/agencyplatformclonecoding/controller/AgentControllerTest.java
@@ -1,23 +1,23 @@
 package com.agencyplatformclonecoding.controller;
 
+import com.agencyplatformclonecoding.config.SecurityConfig;
 import com.agencyplatformclonecoding.service.AgentService;
 import com.agencyplatformclonecoding.service.PaginationService;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@Disabled("구현 중")
 @DisplayName("VIEW 컨트롤러 - 에이전트 관리")
+@Import(SecurityConfig.class)
 @WebMvcTest(AgentController.class)
 class AgentControllerTest {
 
@@ -35,7 +35,6 @@ class AgentControllerTest {
         this.mvc = mvc;
     }
 
-    //@Disabled("구현 중")
     @DisplayName("[VIEW][GET] 에이전트 리스트 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingAgentsView_thenReturnsAgentsView() throws Exception {
@@ -49,7 +48,6 @@ class AgentControllerTest {
                 .andExpect(model().attributeExists("agents"));
     }
 
-    //@Disabled("구현 중")
     @DisplayName("[VIEW][GET] 에이전트 상세 정보 - 정상 호출")
     @Test
     public void givenNothing_whenRequestingAgentDetailView_thenReturnsAgentDetailView() throws Exception {
@@ -60,7 +58,20 @@ class AgentControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
                 .andExpect(view().name("agents/detail"))
-                .andExpect(model().attributeExists("agent"));
+                .andExpect(model().attributeExists("agent"))
+                .andExpect(model().attributeExists("clients"));
     }
 
+    @DisplayName("[VIEW][GET] 에이전트 상세 정보 -> 담당 광고주의 캠페인 관리 페이지로 리다이렉션")
+    @Test
+    void givenNothing_whenRequestingRootPage_thenRedirectsToClientsView() throws Exception {
+        // Given
+
+        // When & Then
+        mvc.perform(get("/agents/agentId/clientId"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("forward:/manage/{clientId}"))
+                .andExpect(forwardedUrl("/manage/{clientId}"))
+                .andDo(MockMvcResultHandlers.print());
+    }
 }
