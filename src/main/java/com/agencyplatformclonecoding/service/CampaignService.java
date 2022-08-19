55b2fc406a8852279a098dfe5b595318f69dee58
mrcocoball <gunmaru93@gmail.com>
2022-08-19 23:15:03 +0900

    #21 - 캠페인, 소재 서비스 임시 구현
    캠페인 / 소재 관리 기능을 위한 서비스 임시 구현

--- src/main/java/com/agencyplatformclonecoding/service/CampaignService.java
+++ src/main/java/com/agencyplatformclonecoding/service/CampaignService.java
@@ -1,4 +1,19 @@
 package com.agencyplatformclonecoding.service;
 
+import com.agencyplatformclonecoding.repository.ClientUserRepository;
+import com.agencyplatformclonecoding.repository.CreativeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class CampaignService {
+
+    private final ClientUserRepository clientUserRepository;
+    private final CreativeRepository creativeRepository;
+
 }
--- src/main/java/com/agencyplatformclonecoding/service/CreativeService.java
+++ src/main/java/com/agencyplatformclonecoding/service/CreativeService.java
@@ -1,4 +1,19 @@
 package com.agencyplatformclonecoding.service;
 
+import com.agencyplatformclonecoding.repository.CampaignRepository;
+import com.agencyplatformclonecoding.repository.CreativeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@RequiredArgsConstructor
+@Transactional
+@Service
 public class CreativeService {
+
+    private final CampaignRepository campaignRepository;
+    private final CreativeRepository creativeRepository;
+
 }
