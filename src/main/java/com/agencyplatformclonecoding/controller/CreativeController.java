f985db75ac9686d154434fc31f076a3587bedd1b
mrcocoball <gunmaru93@gmail.com>
2022-09-04 17:15:54 +0900

    #43 - 광고 캠페인 소재 관리 기능 테스트 및 구현

--- src/main/java/com/agencyplatformclonecoding/controller/CreativeController.java
+++ src/main/java/com/agencyplatformclonecoding/controller/CreativeController.java
@@ -1,31 +1,100 @@
 package com.agencyplatformclonecoding.controller;
 
+import com.agencyplatformclonecoding.domain.constrant.FormStatus;
+import com.agencyplatformclonecoding.dto.CampaignDto;
+import com.agencyplatformclonecoding.dto.ClientUserDto;
+import com.agencyplatformclonecoding.dto.request.CampaignRequest;
+import com.agencyplatformclonecoding.dto.request.CreativeRequest;
+import com.agencyplatformclonecoding.dto.response.CampaignResponse;
+import com.agencyplatformclonecoding.dto.response.CampaignWithCreativesResponse;
+import com.agencyplatformclonecoding.dto.response.ClientUserWithCampaignsResponse;
+import com.agencyplatformclonecoding.dto.response.CreativeResponse;
+import com.agencyplatformclonecoding.service.CampaignService;
 import com.agencyplatformclonecoding.service.CreativeService;
+import com.agencyplatformclonecoding.service.ManageService;
 import com.agencyplatformclonecoding.service.PaginationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping(value = "/manage/{clientId:.+}/campaigns/{campaignId:.+}/creatives", method= RequestMethod.GET)
+@RequestMapping(value = "/manage/{clientId}/campaigns/{campaignId}/creatives")
 @Controller
 public class CreativeController {
 
-    private CreativeService creativeService;
-    private PaginationService paginationService;
+    private final ManageService manageService;
+    private final CampaignService campaignService;
+    private final CreativeService creativeService;
 
-    @GetMapping
-    public String creatives(
-            @PathVariable("clientId") String clientId,
-            @PathVariable("campaignId") Long campaignId, ModelMap map) {
-        map.addAttribute("creatives", List.of());
-        return "manage/creative";
-    }
+	@GetMapping("/form")
+   	public String creativeForm(
+               @PathVariable("clientId") String clientId,
+               @PathVariable("campaignId") Long campaignId,
+               ModelMap map
+    ) {
+		CampaignWithCreativesResponse campaign = CampaignWithCreativesResponse.from(campaignService.getCampaignWithCreatives(campaignId));
+		ClientUserWithCampaignsResponse clientUser = ClientUserWithCampaignsResponse.from(manageService.getClientUserWithCampaigns(clientId));
 
+		map.addAttribute("clientUser", clientUser);
+		map.addAttribute("campaign", campaign);
+   		map.addAttribute("formStatus", FormStatus.CREATE);
+
+   		return "manage/creative-form";
+   	}
+
+   	@PostMapping("/form")
+   	public String createNewCreative(
+               @PathVariable("clientId") String clientId,
+               @PathVariable("campaignId") Long campaignId,
+               CreativeRequest creativeRequest
+    ) {
+   		creativeService.saveCreative(creativeRequest.toDto(CampaignDto.of(campaignId)));
+
+   		return "redirect:/manage/{clientId}/campaigns/{campaignId}/creatives";
+   	}
+
+   	@GetMapping("/{creativeId}/form")
+   	public String updateCreativeForm(
+               @PathVariable("clientId") String clientId,
+               @PathVariable("campaignId") Long campaignId,
+               @PathVariable Long creativeId,
+               ModelMap map
+    ) {
+   		CreativeResponse creative = CreativeResponse.from(creativeService.getCreative(creativeId));
+		CampaignWithCreativesResponse campaign = CampaignWithCreativesResponse.from(campaignService.getCampaignWithCreatives(campaignId));
+		ClientUserWithCampaignsResponse clientUser = ClientUserWithCampaignsResponse.from(manageService.getClientUserWithCampaigns(clientId));
+
+		map.addAttribute("clientUser", clientUser);
+		map.addAttribute("campaign", campaign);
+   		map.addAttribute("creative", creative);
+   		map.addAttribute("formStatus", FormStatus.UPDATE);
+
+   		return "manage/creative-form";
+   	}
+
+   	@PostMapping("/{creativeId}/form")
+   	public String updateCreative(
+			   @PathVariable("clientId") String clientId,
+               @PathVariable("campaignId") Long campaignId,
+			   @PathVariable Long creativeId,
+			   CreativeRequest creativeRequest
+	) {
+   		creativeService.updateCreative(creativeId, creativeRequest.toDto(CampaignDto.of(campaignId)));  // TODO : 추후 에이전시 인증 기능 부여
+
+        return "redirect:/manage/{clientId}/campaigns/{campaignId}/creatives";
+   	}
+
+   	@PostMapping ("/{creativeId}/delete")
+   	public String deleteCampaignId(
+			   @PathVariable("clientId") String clientId,
+               @PathVariable("campaignId") Long campaignId,
+               @PathVariable Long creativeId
+	) {
+   		creativeService.deleteCreative(creativeId);
+
+   		return "redirect:/manage/{clientId}/campaigns/{campaignId}/creatives";
+   	}
 }
--- src/main/java/com/agencyplatformclonecoding/service/CreativeService.java
+++ src/main/java/com/agencyplatformclonecoding/service/CreativeService.java
@@ -1,11 +1,21 @@
 package com.agencyplatformclonecoding.service;
 
+import com.agencyplatformclonecoding.domain.Campaign;
+import com.agencyplatformclonecoding.domain.Creative;
+import com.agencyplatformclonecoding.dto.CreativeDto;
 import com.agencyplatformclonecoding.repository.CampaignRepository;
+import com.agencyplatformclonecoding.repository.ClientUserRepository;
 import com.agencyplatformclonecoding.repository.CreativeRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+import static com.agencyplatformclonecoding.domain.QCampaign.campaign;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -13,7 +23,53 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class CreativeService {
 
-    private final CampaignRepository campaignRepository;
+    private final ClientUserRepository clientUserRepository;
+	private final CampaignRepository campaignRepository;
     private final CreativeRepository creativeRepository;
+
+	@Transactional(readOnly = true)
+	public CreativeDto getCreative(Long creativeId) {
+		return creativeRepository.findById(creativeId)
+				.map(CreativeDto::from)
+				.orElseThrow(() -> new EntityNotFoundException("소재가 존재하지 않습니다 - creativeId : " + creativeId));
+	}
+
+	@Transactional(readOnly = true)
+	public Page<CreativeDto> searchCreatives(Pageable pageable) {
+		return creativeRepository.findAll(pageable).map(CreativeDto::from);
+	}
+
+	public void saveCreative(CreativeDto dto) {
+		try {
+			Campaign campaign = campaignRepository.getReferenceById(dto.campaignDto().id());
+			creativeRepository.save(dto.toEntity(campaign));
+		} catch (EntityNotFoundException e) {
+			log.warn("소재 저장에 실패하였습니다. 소재 저장에 필요한 정보를 찾을 수 없습니다. - {}", e.getLocalizedMessage());
+		}
+	}
+
+	public void updateCreative(Long creativeId, CreativeDto dto) {
+
+		try {
+				Creative creative = creativeRepository.getReferenceById(creativeId);
+				Campaign campaign = campaignRepository.getReferenceById(dto.campaignDto().id());
+
+				if (creative.getCampaign().equals(campaign))
+						if (dto.keyword() != null) {
+							creative.setKeyword(dto.keyword());
+						}
+						creative.setBidingPrice(dto.bidingPrice());
+		} catch (EntityNotFoundException e) {
+			log.warn("소재를 수정하는데 필요한 정보를 찾을 수 없습니다. - dto : {}", e.getLocalizedMessage());
+		}
+	}
+
+	public void deleteCreative(Long creativeId) {
+		creativeRepository.deleteById(creativeId);
+	}
+
+	public long getCreativeCount() {
+		return creativeRepository.count();
+	}
 
 }
