45c17c84f3cf14c586700664b79d911952b22f50
mrcocoball <gunmaru93@gmail.com>
2022-09-04 17:15:14 +0900

    #43 - Repository 수정 및 Request 추가

---
+++ src/main/java/com/agencyplatformclonecoding/dto/request/CreativeRequest.java
@@ -0,0 +1,24 @@
+package com.agencyplatformclonecoding.dto.request;
+
+import com.agencyplatformclonecoding.dto.CampaignDto;
+import com.agencyplatformclonecoding.dto.CreativeDto;
+
+public record CreativeRequest(
+		Long campaignId,
+		String keyword,
+		Long bidingPrice
+) {
+
+	public static CreativeRequest of (Long campaignId, String keyword, Long bidingPrice) {
+		return new CreativeRequest(campaignId, keyword, bidingPrice);
+	}
+
+	public CreativeDto toDto(CampaignDto campaignDto) {
+		return CreativeDto.of(
+				campaignDto,
+				campaignDto.id(),
+				keyword,
+				bidingPrice
+		);
+	}
+}
--- src/main/java/com/agencyplatformclonecoding/repository/CreativeRepository.java
+++ src/main/java/com/agencyplatformclonecoding/repository/CreativeRepository.java
@@ -1,7 +1,30 @@
 package com.agencyplatformclonecoding.repository;
 
+import com.agencyplatformclonecoding.domain.Campaign;
 import com.agencyplatformclonecoding.domain.Creative;
+import com.agencyplatformclonecoding.domain.QCampaign;
+import com.agencyplatformclonecoding.domain.QCreative;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 
-public interface CreativeRepository extends JpaRepository<Creative, Long> {
+public interface CreativeRepository extends
+        JpaRepository<Creative, Long>,
+        QuerydslPredicateExecutor<Creative>,
+        QuerydslBinderCustomizer<QCreative> {
+
+    void deleteById(Long id);
+
+    @Override
+    default void customize(QuerydslBindings bindings, QCreative root) {
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.keyword, root.createdAt, root.createdBy);
+        bindings.bind(root.keyword).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
+
