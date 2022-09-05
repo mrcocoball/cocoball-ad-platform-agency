256622256454122cf67fd66e5abf7475b0d79782
mrcocoball <gunmaru93@gmail.com>
2022-09-05 22:11:46 +0900

    #45 - 인증 기능 및 권한 부여를 위한 작업
    * PlatformPrincipal로 권한 부여
    * SecurityConfig을 수정하여 스태틱 리소스를 제외한 모든 영역에 대해 인증 필요하게 수정
    * 인증의 주체가 되는 에이전시 계정에 대한 도메인, Dto 수정 (비밀번호 누락되어 있었음)

--- src/main/java/com/agencyplatformclonecoding/dto/AgencyDto.java
+++ src/main/java/com/agencyplatformclonecoding/dto/AgencyDto.java
@@ -4,17 +4,19 @@ import com.agencyplatformclonecoding.domain.Agency;
 
 public record AgencyDto(
         String agencyId,
+        String password,
         String agencyName
 ) {
 
-    public static AgencyDto of(String agencyId, String agencyName) {
-        return new AgencyDto(agencyId, agencyName);
+    public static AgencyDto of(String agencyId, String password, String agencyName) {
+        return new AgencyDto(agencyId, password, agencyName);
     }
 
     // Entity -> dto로 변환
     public static AgencyDto from(Agency entity) {
         return new AgencyDto(
 				entity.getAgencyId(),
+                entity.getPassword(),
                 entity.getAgencyName()
         );
     }
@@ -23,6 +25,7 @@ public record AgencyDto(
     public Agency toEntity() {
         return Agency.of(
                 agencyId,
+                password,
 				agencyName
         );
     }
