6f582c7faaeaddbff59505fa11096dc5ed38529f
mrcocoball <gunmaru93@gmail.com>
2022-09-29 23:44:33 +0900

    #87 - 조회/통계 타입 구현
    * 7일 전, 30일 전, 임의기간 조회 3가지 타입으로 지정, 파라미터값으로 받을 예정

---
+++ src/main/java/com/agencyplatformclonecoding/domain/constrant/StatisticsType.java
@@ -0,0 +1,16 @@
+package com.agencyplatformclonecoding.domain.constrant;
+
+import lombok.Getter;
+
+public enum StatisticsType {
+    BEFORE_WEEK("최근 7일"),
+    BEFORE_MONTH("최근 30일"),
+    BEFORE_CUSTOM("기간 설정");
+
+    @Getter
+    private final String description;
+
+    StatisticsType(String description) {
+        this.description = description;
+    }
+}
