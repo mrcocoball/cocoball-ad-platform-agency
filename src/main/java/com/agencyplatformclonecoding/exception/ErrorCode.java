566ee6c4064b6056db8189cb5179159e04d707d7
mrcocoball <gunmaru93@gmail.com>
2022-10-26 00:39:52 +0900

    #104 - 에러 페이지 수정 및 ErrorCode 출력 메시지 수정
    * 에러 페이지 혼자 디자인이 적용되지 않아 붕 뜬 감이 있어 디자인 수정하였음
    * ErrorCode 메시지를 한국어로 변경하여 에러 내용을 쉽게 파악할 수 있게 변경
    * 단, Exception 관련 Response 처리가 제대로 되지 않아 Heroku에서는 에러 메시지가 제대로 표시되지 않을 것으로 예상

--- src/main/java/com/agencyplatformclonecoding/exception/ErrorCode.java
+++ src/main/java/com/agencyplatformclonecoding/exception/ErrorCode.java
@@ -14,18 +14,18 @@ public enum ErrorCode {
 
     OK(HttpStatus.OK, "ok"),
 
-    AGENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Agent not founded"),
-    AGENT_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "Agent Group not founded"),
-    CLIENT_NOT_FOUND(HttpStatus.NOT_FOUND, "Client not founded"),
-    CAMPAIGN_NOT_FOUND(HttpStatus.NOT_FOUND, "Campaign not founded"),
-    CREATIVE_NOT_FOUND(HttpStatus.NOT_FOUND, "Campaign not founded"),
-    PERFORMANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "Performance not founded"),
-    AGENT_EXISTS(HttpStatus.CONFLICT, "Agent exists"),
-    CLIENT_EXISTS(HttpStatus.CONFLICT, "Client exists"),
-    INVALID_RELATION(HttpStatus.CONFLICT, "Client, Campaign, Creative does not match with each other"),
+    AGENT_NOT_FOUND(HttpStatus.NOT_FOUND, "에이전트를 찾을 수 없습니다."),
+    AGENT_GROUP_NOT_FOUND(HttpStatus.NOT_FOUND, "에이전트 그룹을 찾을 수 없습니다."),
+    CLIENT_NOT_FOUND(HttpStatus.NOT_FOUND, "광고주를 찾을 수 없습니다."),
+    CAMPAIGN_NOT_FOUND(HttpStatus.NOT_FOUND, "캠페인을 찾을 수 없습니다."),
+    CREATIVE_NOT_FOUND(HttpStatus.NOT_FOUND, "소재를 찾을 수 없습니다."),
+    PERFORMANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "실적을 찾을 수 없습니다."),
+    AGENT_EXISTS(HttpStatus.CONFLICT, "그룹 내 에이전트가 존재하여 삭제 할 수 없습니다."),
+    CLIENT_EXISTS(HttpStatus.CONFLICT, "매핑된 광고주가 존재하여 삭제할 수 없습니다."),
+    INVALID_RELATION(HttpStatus.CONFLICT, "잘못된 접근입니다."),
 
-    BAD_REQUEST(HttpStatus.BAD_REQUEST, "Bad Request"),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "Internal server error");
+    BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류입니다.");
 
     private HttpStatus status;
     private String message;
