6a0eb3890e2c24c225530db3f4a5be74957384c5
mrcocoball <gunmaru93@gmail.com>
2022-11-10 01:24:55 +0900

    #140 - Dto 수정
    * 실적 지표 정보 및 메소드 추가

--- src/main/java/com/agencyplatformclonecoding/dto/DashboardStatisticsDto.java
+++ src/main/java/com/agencyplatformclonecoding/dto/DashboardStatisticsDto.java
@@ -15,8 +15,24 @@ public class DashboardStatisticsDto {
     String clientId;
     String clientName;
     String category;
+    Long view;
+    String sView;
+    Long click;
+    String sClick;
+    Long conversion;
+    String sConversion;
+    Long purchase;
+    String sPurchase;
     Long spend;
     String sSpend;
+    double CTR;
+    String sCTR;
+    double CVR;
+    String sCVR;
+    Long CPA;
+    String sCPA;
+    double ROAS;
+    String sROAS;
     Long minusVAT;
     String sMinusVAT;
     Long VAT;
@@ -48,10 +64,71 @@ public class DashboardStatisticsDto {
         this.sMinusVAT = sMinusVAT;
     }
 
+    public void setPerformanceIndicator(Long spend, Long view, Long click, Long conversion, Long purchase) {
+
+        String sSpend = formatToString(spend);
+        String sView = formatToString(view);
+        String sClick = formatToString(click);
+        String sConversion = formatToString(conversion);
+        String sPurchase = formatToString(purchase);
+        double CTR = calculateCTR(click, view);
+        String sCTR = String.format("%.2f", CTR * 100);
+        double CVR = calculateCVR(conversion, click);
+        String sCVR = String.format("%.2f", CVR * 100);
+        Long CPA = calculateCPA(spend, conversion);
+        String sCPA = formatToString(CPA);
+        double ROAS = calculateROAS(purchase, spend);
+        String sROAS = String.format("%.2f", ROAS * 100);
+
+        this.sSpend = sSpend;
+        this.sView = sView;
+        this.sClick = sClick;
+        this.sConversion = sConversion;
+        this.sPurchase = sPurchase;
+        this.CTR = CTR;
+        this.sCTR = sCTR;
+        this.CVR = CVR;
+        this.sCVR = sCVR;
+        this.CPA = CPA;
+        this.sCPA = sCPA;
+        this.ROAS = ROAS;
+        this.sROAS = sROAS;
+
+    }
+
     public void setStartDateAndLastDate(LocalDate startDate, LocalDate lastDate) {
 
         this.startDate = startDate;
         this.lastDate = lastDate;
+    }
+
+
+    public static double calculateCTR(Long click, Long view) {
+        if (view == 0 || view == null) {
+            return 0;
+        }
+        return (double) click / view;
+    }
+
+    public static double calculateCVR(Long conversion, Long click) {
+        if (click == 0 || click == null) {
+            return 0;
+        }
+        return (double) conversion / click;
+    }
+
+    public static Long calculateCPA(Long spend, Long conversion) {
+        if (conversion == 0 || conversion == null) {
+            return null;
+        }
+        return spend / conversion;
+    }
+
+    public static double calculateROAS(Long purchase, Long spend) {
+        if (spend == 0 || spend == null) {
+            return 0;
+        }
+        return (double) purchase / spend;
     }
 
     public static String formatToString(Long amount) {
